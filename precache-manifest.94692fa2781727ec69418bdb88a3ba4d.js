098acc4744711187bc94ed4804af8651a526e336
abhishek <aburagad@netapp.com>
2019-04-30 14:09:08 +0530

    Updates

---
+++ precache-manifest.94692fa2781727ec69418bdb88a3ba4d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a66060844696615636d49460ea35b3",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "5139cecc7ad2aefce52d",
+    "url": "/react-game/static/css/2.b6904fd7.chunk.css"
+  },
+  {
+    "revision": "122904377b36103cbd57",
+    "url": "/react-game/static/css/main.342fb2c6.chunk.css"
+  },
+  {
+    "revision": "5139cecc7ad2aefce52d",
+    "url": "/react-game/static/js/2.8d303a40.chunk.js"
+  },
+  {
+    "revision": "122904377b36103cbd57",
+    "url": "/react-game/static/js/main.723781c0.chunk.js"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-game/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-game/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-game/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-game/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-game/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
